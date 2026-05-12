fc4d383523f263fe486934c5643ba3d7d4b32232
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2026-05-12 14:13:39 +0700

    update auth to web and backend

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -8,12 +8,52 @@ import { getProfile } from '../controllers/user.controller.js';
 
 const router = Router();
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+// router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
+//   const user = req.user as UserDocument;
+//   const token = jwt.sign({ userId: user._id }, config.jwt.secret_key, { expiresIn: '7d' });
+//   res.redirect(`${config.app.frontend_url}?token=${token}`);
+// });
+
+router.get('/google', (req, res, next) => {
+  const { redirect_uri } = req.query;
+  
+  const state = redirect_uri 
+    ? Buffer.from(JSON.stringify({ redirect_uri })).toString('base64') 
+    : undefined;
+
+  passport.authenticate('google', { 
+    scope: ['profile', 'email'],
+    state: state 
+  })(req, res, next);
+});
 
 router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
+  if (!req.user) {
+    return res.redirect(`${config.app.frontend_url}?error=auth_failed`);
+  }
+
   const user = req.user as UserDocument;
   const token = jwt.sign({ userId: user._id }, config.jwt.secret_key, { expiresIn: '7d' });
-  res.redirect(`${config.app.frontend_url}?token=${token}`);
+
+  let finalRedirectUrl = config.app.frontend_url;
+
+  const stateQuery = req.query.state;
+  
+  if (stateQuery && typeof stateQuery === 'string') {
+    try {
+      const stateData = JSON.parse(Buffer.from(stateQuery, 'base64').toString());
+      if (stateData.redirect_uri && typeof stateData.redirect_uri === 'string') {
+        finalRedirectUrl = stateData.redirect_uri;
+      }
+    } catch (e) {
+      console.error("Gagal parse state:", e);
+    }
+  }
+
+  const separator = finalRedirectUrl.includes('?') ? '&' : '?';
+  res.redirect(`${finalRedirectUrl}${separator}token=${token}`);
 });
 
 router.get('/profile', authMiddleware, getProfile);
