26ac534448fb5e64da954560b482087dcfa55281
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2026-05-19 18:51:16 +0700

    fix: fix bug in profile

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,53 +2,37 @@ import type { Request, Response } from 'express';
 import type { UserDocument } from '../models/user.model.js';
 import { UserService } from '../services/user.service.js';
 
-export class UserController {
-  private userService = new UserService();
+const userService = new UserService();
 
-  getProfile = async (req: Request, res: Response) => {
-    try {
-      const user = req.user;
-      if (!user) {
-        return res.status(401).json({
-          status: 'error',
-          message: 'User tidak terautentikasi',
-        });
-      }
+export const getProfile = async (req: Request, res: Response) => {
+  const user = req.user;
+  const profileData = await userService.getProfileWithFinancials(user);
 
-      const profileData = await this.userService.getProfileWithFinancials(user);
+  res.status(200).json({
+    status: 'success',
+    data: user,
+  });
+};
 
-      res.status(200).json({
-        status: 'success',
-        data: profileData,
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 'error',
-        message: 'Gagal mengambil data profil',
-      });
-    }
-  };
+export const updateProfile = async (req: Request, res: Response) => {
+  try {
+    const user = req.user as UserDocument;
+    const { name, avatar_url } = req.body;
 
-  updateProfile = async (req: Request, res: Response) => {
-    try {
-      const user = req.user as UserDocument;
-      const { name, avatar_url } = req.body;
+    const updatedUser = await userService.updateUserService(user._id.toString(), {
+      name,
+      avatar_url
+    });
 
-      const updatedUser = await this.userService.updateUserService(user._id.toString(), {
-        name,
-        avatar_url,
-      });
-
-      res.status(200).json({
-        status: 'success',
-        message: 'Profil berhasil diperbarui',
-        data: updatedUser,
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 'error',
-        message: 'Gagal memperbarui profil',
-      });
-    }
-  };
-}
+    res.status(200).json({
+      status: 'success',
+      message: 'Profil berhasil diperbarui',
+      data: updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Gagal memperbarui profil',
+    });
+  }
+};
