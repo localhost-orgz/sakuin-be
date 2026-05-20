347bb7bdfad562bf4064872b480ea42ec52cfc31
Badzlan Nur Dhabith <badzlandhabith05@gmail.com>
2026-05-20 16:09:11 +0700

    feat: delete cascade wallet

--- src/models/wallet.model.ts
+++ src/models/wallet.model.ts
@@ -1,4 +1,5 @@
 import { model, Schema, type HydratedDocument, type InferSchemaType } from 'mongoose';
+import { TransactionModel } from './transaction.model.js';
 
 const schema = new Schema({
   user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
@@ -7,6 +8,20 @@ const schema = new Schema({
   name: { type: String, required: true },
   color: { type: String, required: true },
   balance: { type: Number, required: true, default: 0 },
+});
+
+schema.pre('findOneAndDelete', async function (this: any) {
+  try {
+    const query = this.getQuery();
+    const wallet = await this.model.findOne(query);
+
+    if (wallet) {
+      await TransactionModel.deleteMany({ wallet_id: wallet._id });
+    }
+  } catch (error) {
+    console.error('Terjadi kesalahan saat cascade delete transaksi:', error);
+    throw error;
+  }
 });
 
 export type Wallet = InferSchemaType<typeof schema>;
